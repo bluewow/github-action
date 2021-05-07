fd2eed54436c5d5616240e1555e5582eca27c32f
bluewow <dogseen@gmail.com>
2021-05-07 11:49:47 +0900

    git push example

---
+++ src/main/java/com/example/githubaction/TestController.java
@@ -0,0 +1,15 @@
+package com.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+    @GetMapping("/A")
+    public String test() {
+        System.out.println("TEST-A");
+        return "TEST-A";
+    }
+}
